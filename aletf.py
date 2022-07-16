a426539b19cbb40a01cb00f4c583a1e07f9c2cac
User-max900 <82824533+User-max900@users.noreply.github.com>
2022-07-16 14:53:45 +0700

    Update aletf.py

--- aletf.py
+++ aletf.py
@@ -120,12 +120,9 @@ def main():
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             recx,recy,recw,rech = cv2.boundingRect(c)
-        #    print ("a="+str(recx)+"b="+str(recy)+"c="+str(recw)+"d="+str(rech))
-         #   print ("x="+str(x)+"y="+str(y)+"rad="+str(radius))
 
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-    #       print ("p1="+str(M["m00"])+"p2="+str(M["m01"])+"p3="+str(M["m10"]))
             # only proceed if the radius meets a minimum size
             if radius > 10:
                 # draw the circle and centroid on the frame,
